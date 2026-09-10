2b551b6c79eba29c3a778fddb873a2e542086389
Chinna Govindu Jada <jadachinnagovindu90@gmail.com>
2026-09-10 22:44:35 +0530

    array problems practice

---
+++ javapractice/src/com/javapractice/Practice4.java
@@ -0,0 +1,92 @@
+package com.javapractice;
+
+public class Practice4 {
+	
+
+	public static void main(String[] args) {
+		
+//		**********Reverse Array In Place***********
+//		int arr[]= {10,20,30,40,50};
+//		int start=0;
+//		int end=arr.length-1;
+//		while(start<end)
+//		{
+//			int temp=arr[start];
+//			arr[start]=arr[end];
+//			arr[end]=temp;
+//			start++;
+//			end--;
+//		}
+//		
+//		for(int a:arr)
+//		{
+//			System.out.println(a);
+//		}
+//			
+			
+//		**********Rotate Array left by one position ***********
+		
+		
+//		int arr[]= {10,20,30,40,50};
+//		int first=arr[0];
+//		for(int i=0;i<arr.length-1;i++)
+//		{
+//			arr[i]=arr[i+1];
+//			
+//		}
+//		arr[arr.length-1]=first;
+//		for(int a:arr)
+//		{
+//			System.out.println(a);
+//		}
+//		
+
+		
+//		**********difference b/w max and min an array ***********
+//		int arr[]= {10,50,20,90,40};
+//		int max=arr[0];
+//		int min=arr[0];
+//		for(int a:arr) 
+//		{
+//			if(a>max)
+//			{
+//				max=a;
+//			}
+//			if(a<min)
+//			{
+//				min=a;
+//			}
+//			
+//		}
+//		int diff=max-min;
+//		System.out.println(diff);
+		
+//		**********realtime-Scenario-EmployeeSalaries ***********
+		
+//		double salaries[]= {45000,55000,60000,48000,75000};
+//		double total=0;
+//		double max=salaries[0];
+//		double min=salaries[0];
+//		for(double a:salaries)
+//		{
+//			total+=a;
+//			if(a>max)
+//			{
+//				max=a;
+//			}
+//			if(a<min)
+//			{
+//				min=a;
+//			}
+//			
+//			
+//		}
+//		double avg=total/salaries.length;
+//		System.out.println("total salary "+total);
+//		System.out.println("max salary "+max);
+//		System.out.println("min salary "+min);
+//		System.out.println("avg salary "+avg);
+		
+	}
+
+}
